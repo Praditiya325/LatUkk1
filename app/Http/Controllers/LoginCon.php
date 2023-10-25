bfd2de4a098718817e31ae3a449a6101c9b8bc78
unknown <pradityapratama525@gmail.com>
2023-10-25 10:55:28 +0700

    first commit

---
+++ app/Http/Controllers/LoginCon.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+
+class LoginCon extends Controller
+{
+    public function login()
+    {
+        if (Auth::check()) {
+            return redirect('dashboard');
+        } else {
+            return view('login');
+        }
+    }
+    public function actionlogin(Request $request)
+    {
+        $data = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ];
+        if (Auth::Attempt($data)) {
+            return redirect('dashboard')->with('message', 'Welcome !!!');
+        } else {
+            return redirect('/login')->with('error','Email atau Password Salah');
+        }
+    }
+    public function actionlogout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
